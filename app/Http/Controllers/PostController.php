f621cb13e1684948a112afb61cf65047f331e375
n-ota <n-ota@k-imaging.jp>
2022-10-16 08:31:49 +0900

    Database Transaction

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -5,6 +5,7 @@ namespace App\Http\Controllers;
 use App\Models\Post;
 use Illuminate\Http\Request;
 use Illuminate\Http\JsonResponse;
+use Illuminate\Support\Facades\DB;
 
 class PostController extends Controller
 {
@@ -30,11 +31,15 @@ class PostController extends Controller
      */
     public function store(Request $request)
     {
-        $created = Post::query()->create([
-            'title' => $request->title,
-            'body' => $request->body,
-        ]);
-
+        $created = DB::transaction(function() use ($request){
+            $created = Post::query()->create([
+                'title' => $request->title,
+                'body' => $request->body,
+            ]);
+    
+            $created->users()->sync($request->user_ids);
+            return $created;
+        });
         return new JsonResponse([
             'data' => $created,
         ]);
